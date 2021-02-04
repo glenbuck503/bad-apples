3ef5c960ec53e120d3a82c83865d0bc8e0a0d990
glenbuck503 <glenbuck503@gmail.com>
2021-02-04 13:27:57 -0800

    second prompt workring

--- src/main.js
+++ src/main.js
@@ -16,22 +16,20 @@ $(document).ready(function() {
     game.action = $("select#direction").val();
     game.gameAction();
 
-    $(".playerStats").show();
-
     $(".prompt1").hide();
     $(".prompt2").show();
-    game.action = $("select#door").val();
-    game.gameAction2();
+  
   });
 
   $('form#prompt2').submit(function(event) {
     event.preventDefault();
     let game = new Game();
+    
 
     game.action = $("select#door").val();
     game.gameAction2();
     $(".prompt1").hide();
     $(".prompt2").show();
-  
+
   });
 });
